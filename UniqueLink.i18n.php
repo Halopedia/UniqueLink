44b827577cffde2a4f7bd22c518b1599435c6a74
Daniel Beard <dab1001@hotmail.co.uk>
2020-06-08 00:44:27 +0100

    Add magic word file

---
+++ UniqueLink.i18n.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Internationalization file for the UniqueLink extensions's required magic
+ * words.
+ *
+ * @file UniqueLink.i18n.php
+ * @author Daniel Beard
+ */
+
+$magicWords = [];
+
+$magicWords[ 'en' ] = [
+  'uniquelink' => [ 0, 'uniquelink' ],
+  'uniquelinkifexists' => [ 0, 'uniquelinkifexists' ],
+  'alreadylinkeduniquely' => [ 0, 'alreadylinkeduniquely' ]
+];
+
+?>
